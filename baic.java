1ad111f4876bcaabd2ae331b68d84289577bda5c
Sawepeter <sawepeter6@gmail.com>
2017-09-11 05:03:02 -0700

    is the progress bar main class
    it is the one that makes the progress bar to be visible

---
+++ baic.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package basics;
+
+/**
+ *
+ * @author Peter sawe
+ */
+public class baic {
+     /**
+     *
+     * @param args
+     */
+    public static void main(String[]args){
+            progress m = new progress();
+            m.setVisible(true);
+            m.iterate();
+        }
+}
